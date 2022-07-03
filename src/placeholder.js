b68c57ec6f50065463467caa7b985fdd10304b57
ouanling <rkt1337@gmail.com>
2022-07-03 17:15:57 -0400

    made placeholder more serious

--- src/placeholder.js
+++ src/placeholder.js
@@ -1,30 +1,30 @@
 
 
 const placehold = {
-    name: "Johnstun Troodoo",
-    title: "Make up Artist",
+    name: "Elvis Graton",
+    title: "Star ultime",
     phone: "450-555-3355",
-    mail: "9lives@cantgetfired.com",
-    city: "Tinder, Qc",
+    mail: "think@big.ostie",
+    city: "Hochelaga, Qc",
     intro: "Cupidatat sunt anim incididunt nisi labore sunt nulla Lorem elit irure. Aliquip quis excepteur et nostrud enim irure nostrud officia. Et deserunt et aliquip voluptate elit cupidatat. Adipisicing enim minim do anim eiusmod est. Irure laboris anim voluptate proident. Cillum reprehenderit est magna minim. Nostrud ex aute laborum ea irure amet ea ipsum ut non minim anim nisi.",
     work: [
         {
             id: "1",
-            job: "Supreme leader",
-            jobplace: "country of fakeness",
+            job: "influencer",
+            jobplace: "Quebec media",
             jobfrom: "2021",
             jobto: "2022",
-            jobdesc: "Dressing myself up in black paint to celebrate cultures",
+            jobdesc: "Being all that i can be"
         
         }
     ],
     schooling: [
         {   
-            diploma: "Daddy paid for it",
-            school: "Rich people school",
+            diploma: "precious metal accumulator",
+            school: "Aluminium can machine",
             schoolfrom: "2021",
             schoolto: "2022",
-            desc: "I only went to parties all dressed up to support my foreigner compatriotes"
+            desc: "filling the reservoirs of these precious machine with aluminium cans"
         },
     ]
 
